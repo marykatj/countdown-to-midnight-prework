54666bfe894403d591d810c17ee42927d6434509
Mary-Kate James <marykatj@gmail.com>
2018-04-22 16:55:34 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,8 +12,8 @@ end
 def countdown_with_sleep(integer)
   while integer > 0
     puts "#{integer} SECOND(S)!"
-    sleep(1 second)
-    integer -= 1 
+    sleep(1)
+    integer -= 1
   end
   return "HAPPY NEW YEAR!"
 end
